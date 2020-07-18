bab8bbe528e0ab91b6afcb5552277406a24eb1e2
Gargoth <51285956+Gargoth@users.noreply.github.com>
2020-07-18 23:36:10 +0800

    lorem

--- script-letters.js
+++ script-letters.js
@@ -22,7 +22,7 @@ console.log("functions loaded");
 
 var letters = {
     "text": [
-        '1',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ullam amet, iste recusandae eaque molestiae quo suscipit nobis exercitationem dolores, culpa optio quasi, quam ducimus doloremque dolorem? Debitis, suscipit doloribus eos ipsum cumque exercitationem, vel labore quidem ipsa eaque, quae perferendis atque esse delectus maxime blanditiis adipisci assumenda excepturi eius amet? Accusantium rerum vero dolor aliquid voluptates perferendis eligendi, sunt expedita dolore? Sint reprehenderit facilis maxime debitis dolores maiores animi, id a amet non, officia placeat beatae rerum consequatur vel odit mollitia, nostrum cupiditate dolor! Earum ea atque minima iusto corporis voluptatum alias. Eaque distinctio est dolorum. A illum eaque nesciunt ex iste earum harum sed excepturi molestias quasi quas obcaecati, sequi ratione doloremque nisi voluptatibus dolorem fugit tenetur, aliquid, tempora ea at quod. Doloremque blanditiis repellat praesentium enim. Asperiores, pariatur! Dolores, non. Vero dicta iusto, laboriosam similique excepturi tempore minima. Magnam doloribus sequi necessitatibus. Similique omnis magni dolor quam natus rerum. Culpa veniam quas illum vitae asperiores! Sapiente officia voluptates ea ut animi totam perferendis, quas corporis minus. Excepturi unde quis officiis porro! Eum impedit natus expedita exercitationem, architecto praesentium et. Laboriosam rem atque doloremque autem iste dicta magnam dolorum? Rerum laborum corporis nisi dolores ipsum quasi officia pariatur?',
         '2',
         '3',
         '4'
