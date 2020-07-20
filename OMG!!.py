c4bb0929dd31a441c710a89a4ca3d8b35a9e6543
Gargoth <51285956+Gargoth@users.noreply.github.com>
2020-07-20 16:42:44 +0800

    added letters and fixed intro

--- OMG!!.py
+++ OMG!!.py
@@ -1,39 +1,69 @@
-dialogue = """OMG!!
-you just clicked me *blushes*
-that’s the first time anyone’s ever clicked me! (this is a sex joke omg feel free to take this out)
-AYIEEEEEEEE
-my name is slime-chan!
-and slime-chan is here to make your day the bestest day ever!
-*in quarantine*
-…
-*ahem*
-but enough about me,
-it’s you!
-you’re the fabled Angela Baticulon Napenas!
-and today you’re turning…
-*check’s notes*
-19!!
-*wow, slime-chan could’ve sworn she was only 13*
-W-what? You didn’t hear that!!
-*ahem* ANYWAYS
-HAPPY BIRTHDAY ANGELA!!
-OOH!
-I have a little something for you
-Slime-chan received a mailbox full of letters addressed to you and only you!
-are you ready to read them?
-you know you want to 
-you cant resist the temptation for long!
-BUT BEFORE YOU GO
-do you want to here a joke?
-YES YOU DO!!!
-THE JOKE IS…….
-us.
-we are all jokes.
-.
-*ahem*
-Here are your letters!""".split("\n")
+# dialogue = """*giggles*
+# Hehe, that tickles!
+# *squirms
+# Stop!
+# O-oh wait! It’s you! You’re here! 
+# Let me introduce myself! I’m Slime-chan! 
+# And I’m here to make your birthday the bestest, slimiest one yet!
+# Wait.
+# That came out wrong
+# ...
+# *ehem*
+# So anyways! 
+# Before I get into it! Lemme tell you a joke!
+# Quick, []
+# Get it?! 
+# Great joke right?
+# *but slime chan thinks nothing can be a bigger joke than the ph government*
+# Hehe, I’m cute AND woke!
+# Much like someone else I know....
+# YOU!
+# Yes, you!
+# The great Angela Baticulon Napeñas! 
+# And today you’re turning
+# *checks notes*
+# 19!!!
+# Woah!!!! 
+# *huh. slime chan couldve sworn she was just 13*
+# W-what?? You didn’t hear that!!
+# *blushes in embarrassment*
+# B-but take it as a compliment? 
+# *puppy eyes*
+# Anyway!
+# Guess what!
+# I have a little something for you!
+# Slime-chan received a mailbox full of letters addressed to you and only you!
+# *gets quiet*
+# A-and...don’t tell anyone but...
+# Slime chan 
+# may have accidentally 
+# Read some of them
+# 👉👈 
+# A-and...slime chan thinks that Angela’s friends all love her
+# And are thankful for her being in their life...
+# A-and are happy to see her grow into an amazing person ...
+# B-but enough about that!!
+# I’ll let you read them yourself now!!!
+# Here are your letters!! Slime chan hopes you have a great birthday!!!""".split("\n")
 
-for i in dialogue:
-    i = '"' + i + '",\n' 
+# for i in dialogue:
+#     i = "'" + i + "',\n"
 
-print(dialogue)
+# print(dialogue)
+
+letter = r"""
+Heyy it's your favorite person in the ASHS here! So yeah, lots of stuff going on, I hope you're enjoying your birthday as well as what we've prepared for you HUEHUEHUE! (especially the website i worked hard on it, buti na lang tumulong si harvey goddamn im so proud of that website. especially the whole 'pink' aesthetic grrrr and the pixel art fbasndjkdfhjkd i hope you like slime-chan as much as i do HAHAHDJAHSKJHSJS it been worked on for at least 3 days)
+
+HAAAPPYYYYY BIIIRTTHHHDAYYYY SA IYOO SEEEENGGBOSSSSS! ATEEE ANGELAAA!
+
+You've always been someone who kept me coming back to school, it just do be like that you know? I mean ofc, I really had no choice but to go back to school, that's just how it works, but i have spent A LOT of my recess and lunch times going to Walpole (and getting mildly disappointed because you are a busy hamster). Those after school kwentuhans at the food place near your dorms were always something I looked forward to even when they are unplanned, it's usually just a feeling na 'uy mukhang kakain ulit kami ni angela ah' ya know? I ain't sure if it's just me who gets that feeling HAHAHAHA.
+
+Wala ako masyadong kausap talaga dito sa condo so I think and look back on the days when di ako nauubusan ng kausap HAHAHAHA, and madalas ko talaga naaalala yung walang katapusang inside jokes natin, the heavy stuff we once worried so much about, etc (usually ginagamit ko 'etc' kapag wala na akong maisip na ibang example pero sadyang marami lang talaga dito HAHAHAHAH). Yea so what I wanted to say in this paragraph is that the time I spent with you was just really fun even if it was just two short years. Time is wack ya know, it's so fast when you're having fun or when you're looking back on experiences from the future, parang isang 'WOAH PARE' lang tapos na agad Ateneo life natin woah pare.
+
+So yeaaa, IT EEZ WHAT IT EEZ, you do be old sometimes. Relatively, you are an ancient one. Right now you can finally look down on us, standing atop your pedestal of old age, seemingly one or more units higher than what the rest of us have. HAPPY BIRTHDAY ULIT HOPE WE STILL MEET UP A LOT IN UPD (ofc after quarantine na HEKHEKHEKHEK)!
+
+"""
+letter = letter.split('\n')
+for i in letter:
+    # print("'" + i + "<br>' + ", end='')
+    print('"' + i + '<br>" + ', end='')
